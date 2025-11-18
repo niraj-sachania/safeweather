74a1eb5cd8ff330b768676a9af2e88bfdfdc875e
niraj-sachania <>
2025-11-18 15:02:57 +0000

    refactor: simplify getWeatherData function and remove unused HTML elements

--- assets/js/weather-data.js
+++ assets/js/weather-data.js
@@ -1,10 +1,16 @@
-const getWeatherData = async (lat, lon, apiKey) => {
+const getWeatherData = async () => {
+  // Get latitude and longitude query parameters
+  const queryString = window.location.search;
+  const searchParams = new URLSearchParams(queryString);
+  const lat = searchParams.get("lat");
+  const lon = searchParams.get("lon");
+
   try {
     const weatherData = await fetch(
       `https://strong-mermaid-23f3ab.netlify.app/.netlify/functions/getWeatherData?lat=${lat}&lon=${lon}`
     );
+
     const data = await weatherData.json();
-    console.log(data);
 
     return data;
   } catch (error) {
@@ -13,4 +19,9 @@ const getWeatherData = async (lat, lon, apiKey) => {
   }
 };
 
-const data = getWeatherData("50", "50");
+// Get weather data from our secure proxy server
+(async () => {
+  const data = await getWeatherData();
+
+  console.log(data);
+})();
